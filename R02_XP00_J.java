d4728846344187967b630e133238bceb168221ca
mercpem <me716499@ucf.edu>
2024-10-06 14:39:33 -0400

    Add files via upload

---
+++ R02_XP00_J.java
@@ -0,0 +1,12 @@
+// Rule 02. Expressions (EXP)
+
+
+public void deleteFile()
+{
+	File someFile = new File("someFileName.txt");
+	// Do something with someFile
+	if (!someFile.delete())
+	{
+	// Handle failure to delete the file
+	}
+}
