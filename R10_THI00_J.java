37a1d7651ee1f465395509b9538ce32a870e1bef
mercpem <me716499@ucf.edu>
2024-10-06 19:02:25 -0400

    Add files via upload

---
+++ R10_THI00_J.java
@@ -0,0 +1,12 @@
+// Rule 10. Thread APIs (THI)
+
+public final class Foo implements Runnable {
+  @Override public void run() {
+    // ...
+  }
+ 
+  public static void main(String[] args) {
+    Foo foo = new Foo();
+    new Thread(foo).run();
+  }
+}
