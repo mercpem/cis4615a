9b8884d64b72db9ce9e75fcf72393ad466fa34f4
mercpem <me716499@ucf.edu>
2024-10-06 11:00:35 -0400

    Add files via upload

---
+++ Cis4615a.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac Cis4615a.java
+ *  Execution:    java Cis4615a
+ *
+ *  Prints 
+ *    "Hello, World - Mercedes Pembroke - Fall 2022 -UCF _Mercedes Pembroke_".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java Cis4615a
+ *  Hello, World - Mercedes Pembroke - Fall 2022 - UCF
+ *  %
+ *
+ ******************************************************************************/
+
+public class Cis4615a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - Mercedes Pembroke - Fall 2022 - UCF");
+    }
+
+}
